29c527432ba2416d178be014c8f3462583e16eee
EL MAHDI BENZINA <benzina.elmahdi@gmail.com>
2022-02-09 10:32:20 +0100

    Initial commit

---
+++ src/test/java/com/tuto/jwttuto/JwtTutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuto.jwttuto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtTutoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
