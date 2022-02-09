da53057e8de46bdf093a74b8c7a5ac6441a47288
EL MAHDI BENZINA <benzina.elmahdi@gmail.com>
2022-02-09 12:20:37 +0100

    FEATURE/PO-I3 : generate jwt token

--- src/main/java/com/tuto/jwttuto/configuration/SecurityConfiguration.java
+++ src/main/java/com/tuto/jwttuto/configuration/SecurityConfiguration.java
@@ -2,8 +2,11 @@ package com.tuto.jwttuto.configuration;
 
 import com.tuto.jwttuto.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -19,5 +22,21 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         auth.userDetailsService(userService);
     }
 
+    @Override
+    @Bean
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf()
+                .disable()
+                .authorizeRequests()
+                .antMatchers("/authenticate")
+                .permitAll()
+                .anyRequest()
+                .authenticated();
+
+    }
 }
--- src/main/java/com/tuto/jwttuto/controller/AppController.java
+++ src/main/java/com/tuto/jwttuto/controller/AppController.java
@@ -1,13 +1,54 @@
 package com.tuto.jwttuto.controller;
 
+import com.tuto.jwttuto.model.JwtRequest;
+import com.tuto.jwttuto.model.JwtResponse;
+import com.tuto.jwttuto.service.UserService;
+import com.tuto.jwttuto.utility.JwtUtility;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AppController {
 
+    @Autowired
+    private JwtUtility jwtUtility;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/")
     public String index() {
         return "welcome to jwt tuto";
+    }
+
+    @PostMapping("/authenticate")
+    public JwtResponse authenticate(@RequestBody JwtRequest jwtRequest) throws Exception {
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            jwtRequest.getUsername(),
+                            jwtRequest.getPassword()
+                    )
+            );
+        } catch (BadCredentialsException e){
+            throw new Exception("INVALID_CREDENTIALS",e);
+        }
+        final UserDetails userDetails
+                =userService.loadUserByUsername(jwtRequest.getUsername());
+        final String token =
+                jwtUtility.generateToken(userDetails);
+
+        return new JwtResponse(token);
+
     }
 }
